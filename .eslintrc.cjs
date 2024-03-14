7aea4259a6316ad6e9f58be4c55440717846b214
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-03-14 11:28:02 +0900

    Revert "fix: 삭제된 페이지들을 import에서 제거 (#9)" (#10)
    This reverts commit cd6161165357af2f22a4133b153587bb211fd94e.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,7 +22,5 @@ module.exports = {
   plugins: ["react"],
   rules: {
     "react/react-in-jsx-scope": 1,
-    "react/prop-types": 1,
-    "no-unused-vars": 1,
   },
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from './router/router';
-import './input.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
+import "./input.css";
 
-import { Provider } from 'react-redux';
-import store from './store/store';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
-  </Provider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
