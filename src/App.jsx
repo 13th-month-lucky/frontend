c9006ddd1ef544c9c560c930ad5caec08136567f
Kim Yuhyeon <37354574+youhyeoneee@users.noreply.github.com>
2024-03-14 11:27:28 +0900

    Revert "fix: 삭제된 페이지들을 import에서 제거 (#9)"
    This reverts commit cd6161165357af2f22a4133b153587bb211fd94e.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <div>Root Directory</div>
+      <FindoutPage></FindoutPage>
     </>
   );
 }
--- src/pages/products/findout/page.jsx
+++ src/pages/products/findout/page.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import HotIssuePage from "./hotIssue/page";
+
 export default function FindOutPage() {
-  return (
-    <div>
-      <h1>findoutPage</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>findoutPage</h1>
+
+            <HotIssuePage />
+        </div>
+    );
 }
