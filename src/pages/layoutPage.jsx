afd5cf425569b05b248b1b77823898112ba28e9f
youhyeoneee <khukyh2018@khu.ac.kr>
2024-03-21 14:38:22 +0900

    design: 글씨체 변경

--- src/pages/layoutPage.jsx
+++ src/pages/layoutPage.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 export default function MainLayout() {
   return (
     <div
-      className="font-['SpoqaHanSansNeo'] tracking-wide"
+      className="font-['YOON-Gothic-normal'] tracking-wide"
       style={{
         display: "flex",
         flexDirection: "column",
