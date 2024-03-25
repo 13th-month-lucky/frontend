fe624ac175c609d459c038c944d105168dbbf689
allllfo <mirae4292@gmail.com>
2024-03-26 08:40:14 +0900

    feat: 월세, 주택공제

--- src/components/Preview/MonthAndHouse.jsx
+++ src/components/Preview/MonthAndHouse.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Accordion,
   Checkbox,
@@ -7,14 +7,19 @@ import {
   Radio,
   Card,
 } from "flowbite-react";
-import Address from "./Address"; // Import Address component
 import houseImg from "~/assets/images/preview/house.png";
 
-const MonthAndHouse = () => {
+const MonthAndHouse = ({ updateTotal }) => {
   const [checkLoan, setCheckLoan] = useState(false);
-  const [checkMonthly, setCheckMonthly] = useState(false);
-  const [address, setAddress] = useState("");
-  const [addressDetail, setAddressDetail] = useState("");
+  const [checkMonthly, setCheckMonthly] = useState(false); // 월세 체크
+  const [checkYearly, setCheckYearly] = useState(false); // 전세 체크
+  const [monthlyPay, setMonthlyPay] = useState(0); //월세 금액
+  const [monthResult, setMonthResult] = useState(0); //월세 공제 금액
+  const [housingDepositResult, setHousingDepositResult] = useState(0); // 청약 공제 금액
+  const [loanResult, setLoanResult] = useState(0);
+  const [houseTotalResult, setHouseTotalResult] = useState(0); // 주택 관련 총 공제 금액
+  const salary = 40000000; //총 급여
+  const loan = 10000000;
 
   const handleCheckLoanChange = (event) => {
     setCheckLoan(event.target.checked);
@@ -24,14 +29,70 @@ const MonthAndHouse = () => {
   const handleRadioChange = (event) => {
     if (event.target.value === "monthly") {
       setCheckMonthly(event.target.checked);
-    } else {
+      setCheckYearly(false);
+    } else if (event.target.value === "yearly") {
+      setCheckYearly(event.target.checked);
       setCheckMonthly(false); // 자가 선택 시 checkMonthly 상태를 false로 설정
     }
   };
 
-  const setAddressObj = (obj) => {
-    setAddress(obj.areaAddress);
+  const handleMonthlyPay = (event) => {
+    const value = event.target.value;
+    setMonthlyPay(value);
   };
+
+  const monthlyResultCalculate = () => {
+    let result = 0;
+    if (salary > 55000000 && salary <= 70000000) {
+      result = monthlyPay * 10000 * 12 * 0.17;
+      if (result > 7500000) {
+        result = 7500000;
+      }
+    } else {
+      result = monthlyPay * 10000 * 12 * 0.15;
+    }
+    setMonthResult(result);
+    updateTotal("house", result);
+  };
+
+  const houseResultCalculate = () => {
+    let promise = 200000; // 주택 청약
+    if (promise > 3000000) {
+      //납입액 한도를 넘김 => 300만원이 최대
+      promise = 3000000;
+    }
+    let loanAmount = loan * 0.4;
+    let promiseAmount = promise * 12 * 0.4;
+    let result = 0;
+    if (salary <= 7000000) {
+      if (promiseAmount + loanAmount > 4000000) {
+        result = 4000000;
+        loanAmount = Math.abs(result - loanAmount);
+      } else {
+        result = promiseAmount + loanAmount;
+      }
+      setHousingDepositResult(promiseAmount);
+      setLoanResult(loanAmount);
+    } else {
+      if (loanAmount > 4000000) {
+        result = 4000000;
+        setLoanResult(4000000);
+      } else {
+        result = loanAmount;
+        setLoanResult(loanAmount);
+      }
+    }
+    setHouseTotalResult(result);
+    updateTotal("house", result);
+  };
+
+  useEffect(() => {
+    if (checkMonthly) {
+      monthlyResultCalculate();
+    } else {
+      houseResultCalculate();
+    }
+  }, [monthlyPay, checkMonthly, checkYearly, housingDepositResult, loanResult]);
 
   return (
     <>
@@ -48,6 +109,24 @@ const MonthAndHouse = () => {
               </div>
             </Accordion.Title>
             <Accordion.Content className="bg-gray-100">
+              <div>
+                {checkMonthly && checkLoan ? (
+                  <div>
+                    <p>월세 공제 시, 약 {monthResult}원 돌려받을 수 있어요!</p>
+                  </div>
+                ) : null}
+                {checkYearly && checkLoan ? (
+                  <div>
+                    <p>
+                      전세 대출 공제 시, 약 {houseTotalResult}원 돌려받을 수
+                      있어요!
+                      <br />
+                      주택청약: {housingDepositResult}
+                      임차차입금 : {loanResult}
+                    </p>
+                  </div>
+                ) : null}
+              </div>
               <Card>
                 <div>
                   <div className="flex justify-between items-center gap-2 mb-3">
@@ -56,7 +135,7 @@ const MonthAndHouse = () => {
                   </div>
                   <div className="flex justify-between items-center gap-2">
                     <Label htmlFor="promotion" className=" mt-1 text-base">
-                      월세 및 대출 보유 여부
+                      월세 및 전세 대출 보유 여부
                     </Label>
                     <Checkbox
                       id="promotion"
@@ -86,18 +165,10 @@ const MonthAndHouse = () => {
                               id="yearly"
                               name="type"
                               value="yearly"
+                              checked={checkYearly}
                               onChange={handleRadioChange}
                             />
                             <Label htmlFor="yearly">전세</Label>
-                          </div>
-                          <div className="flex items-center gap-2">
-                            <Radio
-                              id="own"
-                              name="type"
-                              value="own"
-                              onChange={handleRadioChange}
-                            />
-                            <Label htmlFor="own">자가</Label>
                           </div>
                         </fieldset>
                         {checkMonthly ? (
@@ -105,41 +176,17 @@ const MonthAndHouse = () => {
                             <p className="text-nowrap">월세</p>
                             <div className="flex justify-end items-center gap-2 text-nowrap">
                               <input
-                                type="number"
                                 name="price"
                                 id="price"
                                 className="text-right block w-1/3 rounded-md border-0 py-1.5 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                placeholder="30"
+                                value={monthlyPay}
+                                onChange={handleMonthlyPay}
                               />
                               <span>만원</span>
                             </div>
                           </div>
                         ) : null}
-                        <TextInput
-                          id="address"
-                          type="text"
-                          placeholder="주소"
-                          className="mt-2"
-                          value={address}
-                          onChange={(e) => setAddress(e.target.value)}
-                        />
                       </div>
-
-                      <div className="max-w-md">
-                        <TextInput
-                          id="addressDetail"
-                          type="text"
-                          value={addressDetail}
-                          placeholder="상세주소"
-                          className="mt-2 mb-2"
-                          onChange={(e) => setAddressDetail(e.target.value)}
-                        />
-                      </div>
-
-                      <Address
-                        className="items-end justify-end"
-                        setAddressObj={setAddressObj}
-                      />
                     </div>
                   </div>
                 )}
@@ -153,3 +200,15 @@ const MonthAndHouse = () => {
 };
 
 export default MonthAndHouse;
+
+/*
+ <div className="flex items-center gap-2">
+                            <Radio
+                              id="own"
+                              name="type"
+                              value="own"
+                              onChange={handleRadioChange}
+                            />
+                            <Label htmlFor="own">자가</Label>
+                          </div>
+*/
