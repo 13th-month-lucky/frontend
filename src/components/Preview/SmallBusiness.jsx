810bc8b156cc6db8938b2d1f843ca809c3fc2693
allllfo <mirae4292@gmail.com>
2024-03-27 08:42:42 +0900

    feat: 중소기업 공제 user 데이터 추가

--- src/components/Preview/SmallBusiness.jsx
+++ src/components/Preview/SmallBusiness.jsx
@@ -1,14 +1,23 @@
 import { React, useEffect, useState } from "react";
 import { Accordion, Card, Checkbox } from "flowbite-react";
 import businessBagImg from "~/assets/images/preview/travel-dynamic-color.png";
-function SmallBusiness({ updateTotal }) {
+import moment from "moment";
+function SmallBusiness({ updateTotal, user, myData }) {
   const [smallBusinessJunior, setSmallBusinessJunior] = useState(false);
   const [smallBusinessSenior, setSmallBusinessSenior] = useState(false);
-
+  const [tax, setTax] = useState(0);
+  const [age, setAge] = useState(0);
   useEffect(() => {
-    const tax = 800000; // 소득세 80만원
-    const age = 28;
+    if (myData.nationalTaxService) {
+      setTax(myData.nationalTaxService.cash.amount); // 소득세 =>마이데이터에 추가하기
+    }
+    if (user.birthday) {
+      const birthDateMoment = moment(user.birthday);
+      const currentMoment = moment();
+      setAge(currentMoment.diff(birthDateMoment, "years"));
+    }
     let price = 0;
+
     // totalPeopleNum이 변경될 때마다 totalPrice를 업데이트합니다.
     if ((age <= 35 || age >= 16) && smallBusinessJunior) {
       price = tax * 0.9;
@@ -19,10 +28,10 @@ function SmallBusiness({ updateTotal }) {
       price = 2000000;
     }
     updateTotal("business", price);
-  }, [smallBusinessJunior, smallBusinessSenior]);
+  }, [smallBusinessJunior, smallBusinessSenior, myData, user]);
 
   const checkHandler = (option, optionHandler) => {
-    console.log(option);
+    // console.log(option);
     if (option) {
       optionHandler(false);
     } else {
@@ -39,36 +48,40 @@ function SmallBusiness({ updateTotal }) {
           </div>
         </Accordion.Title>
         <Accordion.Content className="bg-gray-100">
-          <Card>
-            <div className="flex items-center justify-between mb-3">
-              <p className=" text-black dark:text-black-400 text-base mt-3">
-                중소기업에 재직 중이고, <br />
-                재직 기간이 3년 미만이신가요?
-              </p>
-              <Checkbox
-                id="accept"
-                onChange={() =>
-                  checkHandler(smallBusinessSenior, setSmallBusinessSenior)
-                }
-                className="mt-3 h-6 w-6"
-              />
-            </div>
-          </Card>
-          <Card>
-            <div className="flex items-center justify-between mb-3">
-              <p className=" text-black dark:text-black-400 text-base mt-3">
-                중소기업에 재직 중이고, <br />
-                재직 기간이 5년 미만이신가요?
-              </p>
-              <Checkbox
-                id="accept"
-                onChange={() =>
-                  checkHandler(smallBusinessJunior, setSmallBusinessJunior)
-                }
-                className="mt-3 h-6 w-6"
-              />
-            </div>
-          </Card>
+          {age > 34 ? (
+            <Card>
+              <div className="flex items-center justify-between mb-3">
+                <p className=" text-black dark:text-black-400 text-base mt-3">
+                  중소기업에 재직 중이고, <br />
+                  재직 기간이 3년 미만이신가요?
+                </p>
+                <Checkbox
+                  id="accept"
+                  onChange={() =>
+                    checkHandler(smallBusinessSenior, setSmallBusinessSenior)
+                  }
+                  className="mt-3 h-6 w-6"
+                />
+              </div>
+            </Card>
+          ) : null}
+          {age <= 34 && age >= 18 ? (
+            <Card>
+              <div className="flex items-center justify-between mb-3">
+                <p className=" text-black dark:text-black-400 text-base mt-3">
+                  중소기업에 재직 중이고, <br />
+                  재직 기간이 5년 미만이신가요?
+                </p>
+                <Checkbox
+                  id="accept"
+                  onChange={() =>
+                    checkHandler(smallBusinessJunior, setSmallBusinessJunior)
+                  }
+                  className="mt-3 h-6 w-6"
+                />
+              </div>
+            </Card>
+          ) : null}
         </Accordion.Content>
       </Accordion.Panel>
     </Accordion>
