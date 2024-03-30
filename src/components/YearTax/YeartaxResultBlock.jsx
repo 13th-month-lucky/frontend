a1f5092f1c9b55efa7c85c3e8640f142daeb1c14
youhyeoneee <khukyh2018@khu.ac.kr>
2024-03-30 21:33:28 +0900

    feat: 데이터 정제 방식 변경
    - 10000원 단위 추가
    - 보드에서는 역순(최신순으로)
    - 필요한 데이터만 뽑아서 차트 컴포넌트에 넘겨주기

--- src/components/YearTax/YeartaxResultBlock.jsx
+++ src/components/YearTax/YeartaxResultBlock.jsx
@@ -25,34 +25,17 @@ function dateFormat(date) {
 
 export default function YeartaxResultBlock({ result }) {
   const [date, setDate] = useState("");
-  const [salary, setSalary] = useState(0);
-  const [returnMoney, setReturnMoney] = useState(0);
-  const [link, setLink] = useState("");
-  const unit = 10000; // TODO: 단위 할지 말지
 
   useEffect(() => {
-    if (result) {
-      if (result._id) {
-        setLink("/preview/result/detail/" + result._id);
-      }
-      if (result.createdDate) {
-        setDate(dateFormat(new Date(result.createdDate)));
-      }
-
-      if (result.data) {
-        setSalary(result.data.총급여);
-        setReturnMoney(result.data.돌려받는돈);
-      }
-    }
-  }, [result]);
-
+    setDate(dateFormat(new Date(result.createdDate)));
+  }, result.createdDate);
   return (
     <Card>
       <div>
         <div className="flex justify-between items-center">
           <p className="font-bold">{date}</p>
 
-          <Link to={link}>
+          <Link to={"preview/result/detail/" + result.resultId}>
             <button className="bg-blue-500 text-white text-sm p-1 rounded-[12px] font-semibold flex items-center justify-center gap-1">
               <p className="ml-1">자세히 보기</p>
               <FaChevronRight color="white" />
@@ -62,12 +45,12 @@ export default function YeartaxResultBlock({ result }) {
 
         <div className="flex justify-between items-center mt-4 text-gray-500 font-semibold">
           <p>나의 총급여</p>
-          <p>{salary.toLocaleString("kr-Kr")}원</p>
+          <p>{result.총급여.toLocaleString("kr-Kr")}만원</p>
         </div>
 
         <div className="flex justify-between items-center mt-4 text-gray-500 font-semibold">
           <p>돌려받은 돈</p>
-          <p>{returnMoney.toLocaleString("kr-Kr")}원</p>
+          <p>{result.돌려받은돈.toLocaleString("kr-Kr")}만원</p>
         </div>
       </div>
     </Card>
--- src/pages/preview/prevPreviewPage.jsx
+++ src/pages/preview/prevPreviewPage.jsx
@@ -6,15 +6,23 @@ import BlueButton from "~/components/Button/BlueButton";
 import Tip from "~/components/Tip/Tip";
 import { getAllResult } from "~/lib/apis/result";
 import YeartaxResultBlock from "~/components/YearTax/YeartaxResultBlock";
+import YeartaxResultChart from "~/components/YearTax/YeartaxResultChart";
 
 export default function prevPreviewPage() {
   const userState = useSelector((state) => state.user13th);
   const [resultList, setResultList] = useState([]);
-
+  const unit = 10000;
   useEffect(() => {
     getAllResult(userState.userId).then((resp) => {
-      console.log(resp);
-      setResultList(resp);
+      const formattedResultList = resp.map((result) => {
+        return {
+          createdDate: result.createdDate,
+          돌려받은돈: (result.data.돌려받는돈 / unit).toFixed(),
+          총급여: (result.data.총급여 / unit).toFixed(),
+        };
+      });
+
+      setResultList(formattedResultList);
     });
   }, []);
 
@@ -22,8 +30,9 @@ export default function prevPreviewPage() {
     <div className="bg-white h-screen p-4">
       <TopBackBar title="이전 연말정산 결과 보기" />
 
+      <YeartaxResultChart data={resultList} />
       <div className="mt-5 bg-[#F7F8FC] flex flex-col gap-5 p-5">
-        {resultList.map((ele, idx) => (
+        {[...resultList].reverse().map((ele, idx) => (
           <YeartaxResultBlock key={idx} result={ele} />
         ))}
       </div>
