44ae94d8bfa61263f0dd0e450077002697fa8a97
youhyeoneee <khukyh2018@khu.ac.kr>
2024-03-30 21:32:26 +0900

    feat: 연말정산 이전 결과 차트 컴포넌트 추가

---
+++ src/components/YearTax/Chart/LineChart.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  interaction: {
+    mode: "index",
+    intersect: false,
+  },
+  stacked: false,
+  plugins: {
+    title: {
+      display: false,
+      text: "Chart.js Line Chart - Multi Axis",
+    },
+  },
+  scales: {
+    y: {
+      type: "linear",
+      display: true,
+      position: "left",
+    },
+  },
+};
+
+export default function LineChart({ data }) {
+  return <Line options={options} data={data} />;
+}
