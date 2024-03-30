0372848fefb52cb76209555b8986fcc259eded64
youhyeoneee <khukyh2018@khu.ac.kr>
2024-03-30 21:54:29 +0900

    fix: 링크 오타 수정

--- src/components/YearTax/YeartaxResultBlock.jsx
+++ src/components/YearTax/YeartaxResultBlock.jsx
@@ -35,7 +35,7 @@ export default function YeartaxResultBlock({ result }) {
         <div className="flex justify-between items-center">
           <p className="font-bold">{date}</p>
 
-          <Link to={"preview/result/detail/" + result.resultId}>
+          <Link to={"/preview/result/detail/" + result.resultId}>
             <button className="bg-blue-500 text-white text-sm p-1 rounded-[12px] font-semibold flex items-center justify-center gap-1">
               <p className="ml-1">자세히 보기</p>
               <FaChevronRight color="white" />
