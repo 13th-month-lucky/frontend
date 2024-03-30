87b8d1e00ed13ed6ce028d9e42a5ef55a74a3977
youhyeoneee <khukyh2018@khu.ac.kr>
2024-03-31 00:17:52 +0900

    refactor: 자세히 보기 버튼 컴포넌트 분리 및 의존성 배열 오타 수정

--- src/components/YearTax/YeartaxResultBlock.jsx
+++ src/components/YearTax/YeartaxResultBlock.jsx
@@ -1,27 +1,36 @@
 import { Card } from "flowbite-react";
 import React, { useEffect, useState } from "react";
+
+import { formatDateWithDayOfWeek } from "~/lib/utils/dateFormat";
+
 import { FaChevronRight } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-import { formatDateWithDayOfWeek } from "~/lib/utils/dateFormat";
+
+const DetailBlueButton = ({ resultId }) => {
+  return (
+    <Link to={"/preview/result/detail/" + resultId}>
+      <button className="bg-blue-500 text-white text-sm p-1 rounded-[12px] font-semibold flex items-center justify-center gap-1">
+        <p className="ml-1">자세히 보기</p>
+        <FaChevronRight color="white" />
+      </button>
+    </Link>
+  );
+};
 
 export default function YeartaxResultBlock({ result }) {
   const [date, setDate] = useState("");
 
   useEffect(() => {
     setDate(formatDateWithDayOfWeek(new Date(result.createdDate)));
-  }, result.createdDate);
+  }, [result.createdDate]);
+
   return (
     <Card>
       <div>
         <div className="flex justify-between items-center">
           <p className="font-bold">{date}</p>
 
-          <Link to={"/preview/result/detail/" + result.resultId}>
-            <button className="bg-blue-500 text-white text-sm p-1 rounded-[12px] font-semibold flex items-center justify-center gap-1">
-              <p className="ml-1">자세히 보기</p>
-              <FaChevronRight color="white" />
-            </button>
-          </Link>
+          <DetailBlueButton resultId={result.id} />
         </div>
 
         <div className="flex justify-between items-center mt-4 text-gray-500 font-semibold">
